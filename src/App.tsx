352e2a1e104f030e01432117880f08d703ead469
Tasos Tsournos <tasos@moveo.ai>
2023-11-11 17:04:43 +0200

    init 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Reaction from './components/Reaction';
 function App() {
   return (
     <div className="App">
-      <div className='testBox' id='testBox'><Reaction>child</Reaction></div>
+      <div className='testBox' id='testBox'><Reaction/></div>
         
     </div>
   );
--- src/components/Reaction.tsx
+++ src/components/Reaction.tsx
@@ -1,41 +1,82 @@
-import { Button, IconButton, Popover, TextField } from "@mui/material"
-import Card from "@mui/material/Card"
-import { useEffect, useState } from "react"
-import EmojiEmotionIcon from "@mui/icons-material/EmojiEmotions"
+import React, { useState, useEffect } from 'react';
+import {  TextField, Button, IconButton, Box } from '@mui/material';
+import Picker from 'emoji-picker-react';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
-type Props = {
-    children: React.ReactNode
-}
+const Reaction = () => {
+    const [cursorPosition, setCursorPosition] = useState<Record<string, number> | null>(null);
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+    const [text, setText] = useState('');
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
-const Reaction = ({children}: Props) => {
-    const [reaction, setReaction] = useState(null)
-    const [comment, setComment] = useState('')
-    const[anchorEl, setAnchorEl] = useState(null)
-    const [cursorPosition, setCursorPosition] = useState({
-        x: 0,
-        y: 0
-    })
-    console.log(cursorPosition);
-   useEffect(() => {
-    document.getElementById('testBox')?.addEventListener('click', (event) => {  
-        setCursorPosition({
-            x: event.clientX,
-            y: event.clientY
-        })
+    useEffect(() => {
+        const handleClick = (event: any) => {  
+            event.preventDefault();
+            if (event.target.id === 'testBox') {
+                setCursorPosition({
+                    x: event.clientX,
+                    y: event.clientY
+                });
+            }
+        };
+
+        document.getElementById('testBox')?.addEventListener('click', handleClick);
+
+        return () => {
+            document.getElementById('testBox')?.removeEventListener('click', handleClick);
+        };
+    }, []);
+
+    const onEmojiClick = (emojiObject: any, event: any) => {
+        event.stopPropagation();
+        setChosenEmoji(emojiObject);
+        setShowEmojiPicker(false);
+        setText(text + emojiObject.emoji);
+    };
+
+    const toggleEmojiPicker = () => {
+        setShowEmojiPicker(!showEmojiPicker);
+    };
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        setShowEmojiPicker(false);
+        setCursorPosition(null);
+        const testBox = document.getElementById('testBox');
+        const child = document.createElement('span');
+        child.textContent = text;
+        child.style.left = cursorPosition?.x + 'px';
+        child.style.top = cursorPosition?.y + 'px';
+        child.style.position = 'fixed';
+        testBox?.appendChild(child);
+        
+        setText('');
     }
-    )
-    return () => {
-        document.getElementById('testBox')?.removeEventListener('click', () => {})
-    }
-    }, [])
-    
-
 
     return (
-        <Card sx={{left: cursorPosition.x, top: cursorPosition.y, position:'fixed'}}>
-            {children}
-        </Card>
-    )
-}
+        <>
+        {cursorPosition && <Box sx={{ left: cursorPosition?.x, top: cursorPosition?.y, position: 'fixed', display: "flex", gap: '16px', border: '1px solid gray', borderRadius: '8px', padding: "16px" }}>
+            <IconButton onClick={toggleEmojiPicker}>
+                <EmojiEmotionsIcon /> 
+            </IconButton>
+            {showEmojiPicker && (
+                <div className='emoji_picker_wrapper'>
+                    <Picker onEmojiClick={onEmojiClick} width={400} height={400} previewConfig={{showPreview: false}}/>
+                </div>
+            )}
+            <TextField
+                label="Your message"
+                variant="outlined"
+                fullWidth
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+                Send
+            </Button>
+        </Box>}
+        </>
+    );
+};
 
-export default Reaction
+export default Reaction;
