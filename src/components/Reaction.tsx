9636a27c195ed4bb3cba009dba6c14b2fcfb1e62
Tasos Tsournos <tasos@moveo.ai>
2023-11-11 17:13:07 +0200

    init 3

--- src/components/Reaction.tsx
+++ src/components/Reaction.tsx
@@ -2,30 +2,35 @@ import React, { useState, useEffect } from 'react';
 import {  TextField, Button, IconButton, Box } from '@mui/material';
 import Picker from 'emoji-picker-react';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 const Reaction = () => {
     const [cursorPosition, setCursorPosition] = useState<Record<string, number> | null>(null);
-    const [chosenEmoji, setChosenEmoji] = useState(null);
+    const [, setChosenEmoji] = useState(null);
     const [text, setText] = useState('');
     const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
     useEffect(() => {
-        const handleClick = (event: any) => {  
+        const handleClick = (event: MouseEvent) => {  
             event.preventDefault();
-            if (event.target.id === 'testBox') {
+            const target = event.target as HTMLElement;
+            if (target.id === 'testBox') {
                 setCursorPosition({
                     x: event.clientX,
                     y: event.clientY
                 });
             }
         };
-
-        document.getElementById('testBox')?.addEventListener('click', handleClick);
-
+    
+        const testBox = document.getElementById('testBox');
+        testBox?.addEventListener('click', handleClick);
+    
         return () => {
-            document.getElementById('testBox')?.removeEventListener('click', handleClick);
+            testBox?.removeEventListener('click', handleClick);
         };
     }, []);
+    
 
     const onEmojiClick = (emojiObject: any, event: any) => {
         event.stopPropagation();
@@ -53,9 +58,18 @@ const Reaction = () => {
         setText('');
     }
 
+    const handleClose = () => {
+    setCursorPosition(null);
+    setShowEmojiPicker(false);
+    setText('');
+};
+
     return (
         <>
         {cursorPosition && <Box sx={{ left: cursorPosition?.x, top: cursorPosition?.y, position: 'fixed', display: "flex", gap: '16px', border: '1px solid gray', borderRadius: '8px', padding: "16px" }}>
+            <IconButton onClick={handleClose} sx={{ marginLeft: 'auto' }}>
+                <CloseIcon />
+             </IconButton>
             <IconButton onClick={toggleEmojiPicker}>
                 <EmojiEmotionsIcon /> 
             </IconButton>
@@ -69,7 +83,12 @@ const Reaction = () => {
                 variant="outlined"
                 fullWidth
                 value={text}
-                onChange={(e) => setText(e.target.value)}
+                onChange={(e) => {setText(e.target.value)}}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        handleSubmit(e);
+                    }
+                }}
             />
             <Button variant="contained" color="primary" onClick={handleSubmit}>
                 Send
